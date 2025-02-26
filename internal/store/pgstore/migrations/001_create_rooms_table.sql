ea25fa7682d024078be2170044e5cc0f87f2084a
Gisele C <giseledasilvac@gmail.com>
2025-02-25 23:57:37 -0300

    commiting migrations

---
+++ internal/store/pgstore/migrations/001_create_rooms_table.sql
@@ -0,0 +1,13 @@
+-- Write your migrate up statements here
+
+---- create above / drop below ----
+
+-- Write your migrate down statements here. If this migration is irreversible
+-- Then delete the separator line above.
+
+CREATE TABLE IF NOT EXISTS rooms(
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    "theme" VARCHAR(255) NOT NULL Not Commited Yet
+);
+
+DROP TABLE IF EXITS rooms;
